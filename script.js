3726e00cd7d2f653dd8d7624b575afbf8c140828
Nikola <markovi3c.nikola@gmail.com>
2023-01-26 17:25:02 +0100

    I kept trying to make this work with using too little functions and not using proper logic. Now it's done though.

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const resultLabel = document.querySelector("label");
 const numbers = document.querySelectorAll(".number");
 const operatorsContainer = document.querySelector("#operations-container");
 const operators = operatorsContainer.children;
+const equalsButton = document.querySelector("#equal-button");
+const clearButton = document.querySelector("#clear-button");
 
-let operation = null;
-let isSet = false;
+let toggle = false;
 let value1 = null;
 let value2 = null;
+let operation = null;
 
 function operate() {
   switch (operation) {
@@ -14,49 +16,69 @@ function operate() {
       value1 = value1 + value2;
       return value1, (value2 = null);
     case "-":
-      subtract(value1, value2);
-      break;
+      value1 = value1 - value2;
+      return value1, (value2 = null);
     case "*":
-      multiply(value1, value2);
-      break;
+      value1 = value1 * value2;
+      return value1, (value2 = null);
     case "/":
-      divide(value1, value2);
-      break;
+      value1 = value1 / value2;
+      return value1, (value2 = null);
   }
 }
 
+//Let's try with functions.
+
+function printNumber() {
+  if (toggle === false) {
+    toggle = true;
+    resultLabel.innerHTML = this.innerHTML;
+  } else if (toggle === true) {
+    resultLabel.innerHTML += this.innerHTML;
+  }
+}
+
+function getValue() {
+  if (value1 === null) {
+    value1 = Number(resultLabel.innerHTML);
+  } else {
+    value2 = Number(resultLabel.innerHTML);
+  }
+}
+
+function printNew() {
+  resultLabel.innerHTML = value1;
+}
+
 for (i of numbers) {
-  i.addEventListener("click", function () {
-    if (isSet === false) {
-      resultLabel.innerHTML += this.innerHTML;
-    } else {
-      resultLabel.innerHTML += this.innerHTML;
-    }
-  });
+  i.addEventListener("click", printNumber);
 }
 
 for (i of operators) {
   i.addEventListener("click", function () {
-    if (isSet === false) {
-      isSet = true;
-      operation = this.innerHTML;
-      value1 = Number(resultLabel.innerHTML);
-      resultLabel.innerHTML = "";
-    } else if (isSet === true) {
-      value2 = Number(resultLabel.innerHTML);
-      operate();
-      resultLabel.innerHTML = value1;
-      isSet = false;
-    }
+    toggle = false;
+    getValue();
+    operate();
+    printNew();
+  });
+  i.addEventListener("click", function () {
+    operation = this.innerHTML;
   });
 }
 
-/*Clear the currentInput after clicking any operator button.
-Once that is cleared add operator on the screen. Clear the screen completely
-when the next number is inputted.
-*/
+equalsButton.addEventListener("click", function () {
+  if (toggle === false) return;
+  toggle = false;
+  getValue();
+  operate();
+  printNew();
+  operation = null;
+});
 
-/*I can make an array and then loop through that array. 
-And then depending on what the next item is I perform the operation on the previous thing?
-This would make sense.
-*/
+clearButton.addEventListener("click", () => {
+  toggle = false;
+  value1 = null;
+  value2 = null;
+  operation = null;
+  resultLabel.innerHTML = null;
+});
